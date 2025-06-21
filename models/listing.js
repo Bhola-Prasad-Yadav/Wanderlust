4f12427d9fc10b1c3dd0bbb461e77602fba719a5
Bhola-Prasad-Yadav <by3338742@gmail.com>
2025-06-21 18:46:02 +0530

    Add Project Files

---
+++ models/listing.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+const Review = require("./review.js");
+const { type } = require("os");
+const Schema = mongoose.Schema;
+
+const listingSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: String,
+    image: {
+        url: {
+            type: String,
+            required: true
+        },
+        filename: {
+            type: String,
+            required: true
+        }
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        },
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    geometry:{
+    type: {
+        type: String, // Don't do `{ location: { type: String } }`
+        enum: ['Point'], // 'location.type' must be 'Point'
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+   },
+//    category: {
+//        type: String,
+//        enum: ["mountains", "rooms", "arctic", "farms", "Castles", "amazing pools", "trending", "iconic cities"],
+//    }
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
+});
+
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
